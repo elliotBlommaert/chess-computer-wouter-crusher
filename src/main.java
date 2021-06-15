241456dd8c674d004ef8a9db5d14e4de97ba8f24
Elliot <elliot.blommaert@gmail.com>
2021-06-15 10:47:47 +0200

    add knight

--- src/main.java
+++ src/main.java
@@ -2,10 +2,7 @@ import general.BoardState;
 import general.DefaultMove;
 import general.Move;
 import general.Position;
-import pieces.Bishop;
-import pieces.Pawn;
-import pieces.Piece;
-import pieces.Rook;
+import pieces.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
@@ -15,11 +12,11 @@ public class main {
 
     public static void main(String[] args) {
         BoardState boardState = new BoardState();
-        Position oldPosition = new Position(2, 2);
-        Bishop bishop = new Bishop(true, oldPosition);
+        Position oldPosition = new Position(4, 4);
+        Piece bishop = new Knight(true, oldPosition);
         boardState.addPiece(bishop);
 //        boardState.addPiece(new Bishop(true, new Position(1,1)));
-        bishop.getPossibleMoves(boardState);
+        System.out.println(bishop.getPossibleMoves(boardState));
         int i = 0;
 
 
--- src/pieces/Knight.java
+++ src/pieces/Knight.java
@@ -1,9 +1,11 @@
 package pieces;
 
 import general.BoardState;
+import general.DefaultMove;
 import general.Move;
 import general.Position;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Knight extends Piece {
@@ -25,6 +27,77 @@ public class Knight extends Piece {
 
     @Override
     public List<Move> getPossibleMoves(BoardState board) {
-        return null;
+
+        List<Move> possibleMoves = new ArrayList<>();
+
+
+        int column = position.getColumn();
+        int row = position.getRow();
+
+        int upLeftColumn = column - 1;
+        int upLeftRow = row + 2;
+        Piece pieceAtTarget = board.getPieceAt(upLeftColumn, upLeftRow);
+        if (row < 8 && upLeftColumn >= 0 && (pieceAtTarget == null || pieceAtTarget.colorWhite != pieceAtTarget.colorWhite)) {
+            DefaultMove newMove = new DefaultMove(this, new Position(upLeftColumn, upLeftRow));
+            possibleMoves.add(newMove);
+        }
+
+        int leftUpColumn = column - 2;
+        int leftUpRow = row + 1;
+        pieceAtTarget = board.getPieceAt(leftUpColumn, leftUpRow);
+        if (row < 8 && leftUpColumn >= 0 && (pieceAtTarget == null || pieceAtTarget.colorWhite != pieceAtTarget.colorWhite)) {
+            DefaultMove newMove = new DefaultMove(this, new Position(leftUpColumn, leftUpRow));
+            possibleMoves.add(newMove);
+        }
+
+        int upRightColumn = column + 1;
+        int upRightRow = row + 2;
+        pieceAtTarget = board.getPieceAt(upRightColumn, upRightRow);
+        if (row < 8 && upRightColumn < 8  && (pieceAtTarget == null || pieceAtTarget.colorWhite != pieceAtTarget.colorWhite)) {
+            DefaultMove newMove = new DefaultMove(this, new Position(upRightColumn, upRightRow));
+            possibleMoves.add(newMove);
+        }
+
+        int rightUpColumn = column + 2;
+        int rightUpRow = row + 1;
+        pieceAtTarget = board.getPieceAt(rightUpColumn, rightUpRow);
+        if (row < 8 && rightUpColumn < 8  && (pieceAtTarget == null || pieceAtTarget.colorWhite != pieceAtTarget.colorWhite)) {
+            DefaultMove newMove = new DefaultMove(this, new Position(rightUpColumn, rightUpRow));
+            possibleMoves.add(newMove);
+        }
+
+        int downLeftColumn = column - 1;
+        int downLeftRow = row - 2;
+        pieceAtTarget = board.getPieceAt(downLeftColumn, downLeftRow);
+        if (row >= 0 && downLeftColumn >= 0 && (pieceAtTarget == null || pieceAtTarget.colorWhite != pieceAtTarget.colorWhite)) {
+            DefaultMove newMove = new DefaultMove(this, new Position(downLeftColumn, downLeftRow));
+            possibleMoves.add(newMove);
+        }
+
+        int leftDownColumn = column - 2;
+        int leftDownRow = row - 1;
+        pieceAtTarget = board.getPieceAt(leftDownColumn, leftDownRow);
+        if (row >= 0 && leftDownColumn >= 0 && (pieceAtTarget == null || pieceAtTarget.colorWhite != pieceAtTarget.colorWhite)) {
+            DefaultMove newMove = new DefaultMove(this, new Position(leftDownColumn, leftDownRow));
+            possibleMoves.add(newMove);
+        }
+
+        int downRightColumn = column + 1;
+        int downRightRow = row - 2;
+        pieceAtTarget = board.getPieceAt(downRightColumn, downRightRow);
+        if (row >= 0 && downRightColumn >= 0 && (pieceAtTarget == null || pieceAtTarget.colorWhite != pieceAtTarget.colorWhite)) {
+            DefaultMove newMove = new DefaultMove(this, new Position(downRightColumn, downRightRow));
+            possibleMoves.add(newMove);
+        }
+
+        int rightDownColumn = column + 2;
+        int rightDownRow = row - 1;
+        pieceAtTarget = board.getPieceAt(rightDownColumn, rightDownRow);
+        if (row >= 0 && rightDownColumn >= 0 && (pieceAtTarget == null || pieceAtTarget.colorWhite != pieceAtTarget.colorWhite)) {
+            DefaultMove newMove = new DefaultMove(this, new Position(rightDownColumn, rightDownRow));
+            possibleMoves.add(newMove);
+        }
+
+        return possibleMoves;
     }
 }
