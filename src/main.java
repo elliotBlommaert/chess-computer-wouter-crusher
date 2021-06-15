49621a4035808ab225a2c3e80090ccdcf7fa1be3
Yolan Uyttenhove <yuyttenhove@gmail.com>
2021-06-15 10:47:30 +0200

    queen possible moves

--- src/main.java
+++ src/main.java
@@ -2,10 +2,7 @@ import general.BoardState;
 import general.DefaultMove;
 import general.Move;
 import general.Position;
-import pieces.Bishop;
-import pieces.Pawn;
-import pieces.Piece;
-import pieces.Rook;
+import pieces.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
@@ -16,10 +13,11 @@ public class main {
     public static void main(String[] args) {
         BoardState boardState = new BoardState();
         Position oldPosition = new Position(2, 2);
-        Bishop bishop = new Bishop(true, oldPosition);
-        boardState.addPiece(bishop);
+        Queen queen = new Queen(true, oldPosition);
+        boardState.addPiece(queen);
+        System.out.println(boardState);
 //        boardState.addPiece(new Bishop(true, new Position(1,1)));
-        bishop.getPossibleMoves(boardState);
+        System.out.println(queen.getPossibleMoves(boardState));
         int i = 0;
 
 
--- src/pieces/Queen.java
+++ src/pieces/Queen.java
@@ -1,9 +1,11 @@
 package pieces;
 
 import general.BoardState;
+import general.DefaultMove;
 import general.Move;
 import general.Position;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Queen extends Piece {
@@ -27,6 +29,106 @@ public class Queen extends Piece {
 
     @Override
     public List<Move> getPossibleMoves(BoardState board) {
-        return null;
+        List<Move> moves = new ArrayList<>();
+        int startRow = position.getRow();
+        int startColumn = position.getColumn();
+        // Right
+        int i = startColumn + 1;
+        boolean foundPiece = false;
+        while (i < 8 && !foundPiece) {
+            Piece pieceAt = board.getPieceAt(startRow, i);
+            foundPiece = pieceAt != null;
+            if (!foundPiece || pieceAt.colorWhite != this.colorWhite) {
+                moves.add(new DefaultMove(this, new Position(startRow, i)));
+            }
+            i++;
+        }
+        // Left
+        i = startColumn - 1;
+        foundPiece = false;
+        while (i >= 0 && !foundPiece) {
+            Piece pieceAt = board.getPieceAt(startRow, i);
+            foundPiece = pieceAt != null;
+            if (!foundPiece || pieceAt.colorWhite != this.colorWhite) {
+                moves.add(new DefaultMove(this, new Position(startRow, i)));
+            }
+            i--;
+        }
+        // Up
+        i = startRow + 1;
+        foundPiece = false;
+        while (i < 8 && !foundPiece) {
+            Piece pieceAt = board.getPieceAt(i, startColumn);
+            foundPiece = pieceAt != null;
+            if (!foundPiece || pieceAt.colorWhite != this.colorWhite) {
+                moves.add(new DefaultMove(this, new Position(i, startColumn)));
+            }
+            i++;
+        }
+        // Down
+        i = startRow - 1;
+        foundPiece = false;
+        while (i >= 0 && !foundPiece) {
+            Piece pieceAt = board.getPieceAt(i, startColumn);
+            foundPiece = pieceAt != null;
+            if (!foundPiece || pieceAt.colorWhite != this.colorWhite) {
+                moves.add(new DefaultMove(this, new Position(i, startColumn)));
+            }
+            i--;
+        }
+        // Up-right
+        i = startColumn + 1;
+        int j = startRow + 1;
+        foundPiece = false;
+        while (i < 8 && j < 8 && !foundPiece) {
+            Piece pieceAt = board.getPieceAt(i, j);
+            foundPiece = pieceAt != null;
+            if (!foundPiece || pieceAt.colorWhite != this.colorWhite) {
+                moves.add(new DefaultMove(this, new Position(i, j)));
+            }
+            i++;
+            j++;
+        }
+        // Down-right
+        i = startColumn + 1;
+        j = startRow - 1;
+        foundPiece = false;
+        while (i < 8 && j >= 0 && !foundPiece) {
+            Piece pieceAt = board.getPieceAt(i, j);
+            foundPiece = pieceAt != null;
+            if (!foundPiece || pieceAt.colorWhite != this.colorWhite) {
+                moves.add(new DefaultMove(this, new Position(i, j)));
+            }
+            i++;
+            j--;
+        }
+        // Down-left
+        i = startColumn - 1;
+        j = startRow - 1;
+        foundPiece = false;
+        while (i >= 0 && j >= 0 && !foundPiece) {
+            Piece pieceAt = board.getPieceAt(i, j);
+            foundPiece = pieceAt != null;
+            if (!foundPiece || pieceAt.colorWhite != this.colorWhite) {
+                moves.add(new DefaultMove(this, new Position(i, j)));
+            }
+            i--;
+            j--;
+        }
+        // Up-left
+        i = startColumn - 1;
+        j = startRow + 1;
+        foundPiece = false;
+        while (i >= 0 && j < 8 && !foundPiece) {
+            Piece pieceAt = board.getPieceAt(i, j);
+            foundPiece = pieceAt != null;
+            if (!foundPiece || pieceAt.colorWhite != this.colorWhite) {
+                moves.add(new DefaultMove(this, new Position(i, j)));
+            }
+            i--;
+            j++;
+        }
+
+        return moves;
     }
 }
